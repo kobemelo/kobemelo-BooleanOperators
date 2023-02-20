79547147aa34943d8f95ea19d7cf1673f038aa69
kobemelo <jgalarz95@gmail.com>
2023-02-20 22:05:59 +0000

    ExpressionsTest Committed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,7 +14,15 @@ public class Lab {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
+
+            if ( b == true ) {
+
+                return true;
+            } else {
+
         return false;
+
+            }
     }
 
     /**
@@ -23,7 +31,17 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+
+        if ( b == true) {
+
+            return false;
+
+        } else {
+
+            return true;
+        }
+
+       
     }
 
     /**
@@ -33,6 +51,13 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
+
+        if ( b1&&b2 == true) {
+
+            return true;
+
+        }
+
         return false;
     }
 
@@ -43,8 +68,18 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
-    }
 
+        if ( b1|b2 == true){
+
+        return true;
+
+        } else if ( b1&&b2 == false){
+
+        }
+
+    
+ return false; // why do we return false after else-if } ?
 
 }
+}
+
